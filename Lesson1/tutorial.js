e9cb39195e0b8bbc403a895fa235d263ebb04e7c
ObiOneKenobii <jimijnrrruji@gmail.com>
2026-04-08 19:07:07 +0100

    first commit

---
+++ Lesson1/tutorial.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let amount = 1000; // This will cause a type error because "Jimmy" is a string, not a number.
+let Studentname = ["Tunde", "Jimmy", "Sarah"]; // This is an array of strings, which is the correct type for student names.
+let isEnrolled = true; // This variable indicates whether the student is enrolled or not
+console.log(Studentname[0]);
+//# sourceMappingURL=tutorial.js.map
---
+++ Lesson1/tutorial.ts
@@ -0,0 +1,9 @@
+
+
+let amount: number = 1000; // This will cause a type error because "Jimmy" is a string, not a number.
+
+let Studentname: string[] = ["Tunde", "Jimmy", "Sarah"]; // This is an array of strings, which is the correct type for student names.
+
+let isEnrolled: boolean = true; // This variable indicates whether the student is enrolled or not
+
+console.log(Studentname[0]);
